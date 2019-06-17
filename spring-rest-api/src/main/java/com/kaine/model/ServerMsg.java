c0f35794e43e3255b2cb8dc9cc745700f9dbab76
al3xka1n3 <al3xka1n3@gmail.com>
2019-06-18 02:21:12 +0300

    setting in order ServerMsg

--- spring-rest-api/src/main/java/com/kaine/model/ServerMsg.java
+++ spring-rest-api/src/main/java/com/kaine/model/ServerMsg.java
@@ -1,36 +1,20 @@
 package com.kaine.model;
 
+import lombok.Getter;
+
 public class ServerMsg {
 
+    @Getter
     private String welcomeMsg = "Welcome To Spring Integration Http Outbound Example!";
+    @Getter
     private String currentTime;
 
-    public ServerMsg(){
-    }
-
     public ServerMsg(String currentTime){
-        this.setCurrentTime(currentTime);
-    }
-
-    public String getWelcomeMsg() {
-        return welcomeMsg;
-    }
-
-    public void setWelcomeMsg(String welcomeMsg) {
-        this.welcomeMsg = welcomeMsg;
-    }
-
-    public String getCurrentTime() {
-        return currentTime;
-    }
-
-    public void setCurrentTime(String currentTime) {
         this.currentTime = currentTime;
     }
 
     public String toString(){
-        String info = String.format("ServerMsg with welcomeMsg = %s, currentTime = %s", welcomeMsg, currentTime);
-        return info;
+        return String.format("ServerMsg with welcomeMsg = %s, currentTime = %s", welcomeMsg, currentTime);
     }
 
 }
